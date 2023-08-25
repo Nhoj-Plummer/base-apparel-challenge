7ca9cb928dcbdae642a17a43ec2d4377cbf306a2
Nhoj <nhojplummer@yahoo.com>
2023-08-25 14:35:47 -0400

    Making progress

--- src/components/HeroData/herodata.jsx
+++ src/components/HeroData/herodata.jsx
@@ -1,15 +1,18 @@
 import React from "react"
 import HeroSection from "../HeroSection/herosection"
 import "./herodata.css"
+import Logo from "../../assets/logo.svg"
 
 const Herodata = () => {
   return (
     <HeroSection>
       <div className="hero-data_text-container">
-        <div className="hero-data_text-image"></div>
-        <h1>We're Coming Soon</h1>
-        <p></p>
-        <form></form>
+        <div className="hero-data_text-image">
+          <img src={Logo} alt="logo" />
+        </div>
+        <h1 className="hero-data-text-header">We're Coming Soon</h1>
+        <p className="hero-data-text-description"></p>
+        <form className="hero-data-text-form"></form>
       </div>
       <div className="hero-data_image-container"></div>
     </HeroSection>
