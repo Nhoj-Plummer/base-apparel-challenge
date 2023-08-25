2f0e26c1827bf66710d47a98712fb55d7743542a
Nhoj <nhojplummer@yahoo.com>
2023-08-25 09:15:19 -0400

    New compoennt piece being made

--- src/components/HeroSection/herosection.jsx
+++ src/components/HeroSection/herosection.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import "./herosection.css"
 
 const Herosection = props => {
-  return <div className="hero-section-container section-padding">Herosection</div>
+  return <div className="hero-section-container section-padding">{props.children}</div>
 }
 
 export default Herosection
