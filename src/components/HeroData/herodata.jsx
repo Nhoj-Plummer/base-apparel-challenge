e5c4ffcf0dc39b488acdf2981c2e0f3a80a46c54
Nhoj <nhojplummer@yahoo.com>
2023-08-27 12:23:14 -0400

    Learning

--- src/components/HeroData/herodata.jsx
+++ src/components/HeroData/herodata.jsx
@@ -6,15 +6,21 @@ import Logo from "../../assets/logo.svg"
 const Herodata = () => {
   return (
     <HeroSection>
-      <div className="hero-data_text-image">{/* <img src={Logo} alt="logo" /> */}</div>
+      <div className="hero-data_text-image">
+        {/* <img src={Logo} alt="logo" /> */}
+        <h1>1</h1>{" "}
+      </div>
       <div className="hero-data_text-container">
+        <h1>2</h1>
         {/* <h1 className="hero-data-text-header">
           <span>We're</span> <span>Coming Soon</span>
         </h1>
         <p className="hero-data-text-description">Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.</p> */}
-        <form className="hero-data-text-form"></form>
+        {/* <form className="hero-data-text-form"></form> */}
       </div>
-      <div className="hero-data_image-container"></div>
+      <div className="hero-data_image-container">
+        <h1>3</h1>
+      </div>
     </HeroSection>
   )
 }
