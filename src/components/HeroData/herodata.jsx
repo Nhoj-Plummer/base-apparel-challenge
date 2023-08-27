5fd7d25ca1979038c490ac006a7ef480f285d913
Nhoj <nhojplummer@yahoo.com>
2023-08-26 20:59:06 -0400

    Need to figure this out

--- src/components/HeroData/herodata.jsx
+++ src/components/HeroData/herodata.jsx
@@ -6,12 +6,12 @@ import Logo from "../../assets/logo.svg"
 const Herodata = () => {
   return (
     <HeroSection>
-      <div className="hero-data_text-image">
-        <img src={Logo} alt="logo" />
-      </div>
+      <div className="hero-data_text-image"><img src={Logo} alt="logo" /></div>
       <div className="hero-data_text-container">
-        <h1 className="hero-data-text-header">We're Coming Soon</h1>
-        <p className="hero-data-text-description"></p>
+        {/* <h1 className="hero-data-text-header">
+          <span>We're</span> <span>Coming Soon</span>
+        </h1>
+        <p className="hero-data-text-description">Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.</p> */}
         <form className="hero-data-text-form"></form>
       </div>
       <div className="hero-data_image-container"></div>
