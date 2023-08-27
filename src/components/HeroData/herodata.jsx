90ef322953e3d71535f26b56e3d50bef1029358c
Nhoj <nhojplummer@yahoo.com>
2023-08-26 21:33:00 -0400

    Someway Somehow

--- src/components/HeroData/herodata.jsx
+++ src/components/HeroData/herodata.jsx
@@ -6,7 +6,9 @@ import Logo from "../../assets/logo.svg"
 const Herodata = () => {
   return (
     <HeroSection>
-      <div className="hero-data_text-image"><img src={Logo} alt="logo" /></div>
+      <div className="hero-data_text-image">
+        <img src={Logo} alt="logo" />
+      </div>
       <div className="hero-data_text-container">
         {/* <h1 className="hero-data-text-header">
           <span>We're</span> <span>Coming Soon</span>
@@ -14,7 +16,9 @@ const Herodata = () => {
         <p className="hero-data-text-description">Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.</p> */}
         <form className="hero-data-text-form"></form>
       </div>
-      <div className="hero-data_image-container"></div>
+      <div className="hero-data_image-container">
+        
+      </div>
     </HeroSection>
   )
 }
