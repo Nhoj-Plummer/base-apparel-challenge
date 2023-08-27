140a303c104097fe25f4428ff651fc50ce3bf896
Nhoj <nhojplummer@yahoo.com>
2023-08-26 23:27:25 -0400

    mkaking something

--- src/components/HeroData/herodata.jsx
+++ src/components/HeroData/herodata.jsx
@@ -16,9 +16,7 @@ const Herodata = () => {
         <p className="hero-data-text-description">Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.</p> */}
         <form className="hero-data-text-form"></form>
       </div>
-      <div className="hero-data_image-container">
-        
-      </div>
+      <div className="hero-data_image-container"></div>
     </HeroSection>
   )
 }
